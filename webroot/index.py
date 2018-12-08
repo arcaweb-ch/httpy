2584f5ad460939b0927d436f5cc94eeb344c0aa6
L.Conti <43749906+arcaweb-ch@users.noreply.github.com>
2018-12-09 00:09:32 +0100

    A better example

--- webroot/index.py
+++ webroot/index.py
@@ -1,1 +1,15 @@
-print('<h1>It works!</h1>')
+""" 
+
+	Python page eyample
+	See core/http.py for http helper methods
+
+"""
+
+html = '<h1>It works!</h1>'
+html += '<form method="get" action="/"><input name="testvar" value="any text" /><input type="submit" value="Test query string"></form>'
+
+testvar = http.get('testvar')
+if testvar:
+	html += '<h2>You submitted: '+testvar+'</h2>'
+
+print(html)
